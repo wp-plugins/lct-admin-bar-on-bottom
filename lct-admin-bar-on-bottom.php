4c65855dfa38aa645d4b7e9d4fbfa69d065a4df2
ircary <ircary@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-19 19:28:25 +0000

    = 1.2.3 = 	- Updated Globals
    git-svn-id: https://plugins.svn.wordpress.org/lct-admin-bar-on-bottom/trunk@935340 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lct-admin-bar-on-bottom.php
+++ lct-admin-bar-on-bottom.php
@@ -1,7 +1,7 @@
 <?php /*
 Plugin Name: LCT Admin Bar on Bottom
 Plugin URI: http://lookclassy.com/wordpress-plugins/admin-bar-on-bottom/
-Version: 1.2.2
+Version: 1.2.3
 Text Domain: lct-admin-bar-on-bottom
 Author: Look Classy Technologies
 Author URI: http://lookclassy.com/
@@ -21,41 +21,45 @@ GNU General Public License for more details.
 
 
 //Globals
-$lct_G = array();
-$lct_G['editzz']					= 'editzz';
-$lct_G['lct_dash']					= 'lct-admin-bar-on-bottom';
-$lct_G['lct_us']					= 'lct_admin_bar_on_bottom';
-$lct_G['plugin_file']				= __FILE__;
-$lct_G['plugin_dir_url']			= plugin_dir_url( __FILE__ );
-$lct_G['plugin_dir_path']			= plugin_dir_path( __FILE__ );
+$lct_g = new lct_g;
+class lct_g {
+ 	public $editzz						= 'editzz';
+	public $lct_dash					= 'lct-admin-bar-on-bottom';
+	public $lct_us						= 'lct_admin_bar_on_bottom';
 
+	public function __construct() {
+		$this->plugin_file				= __FILE__;
+		$this->plugin_dir_url			= plugin_dir_url( __FILE__ );
+		$this->plugin_dir_path			= plugin_dir_path( __FILE__ );
+	}
+}
 
-add_action( 'admin_init', $lct_G['lct_us'] . '_back_css' );
+add_action( 'admin_init', $lct_g->lct_us . '_back_css' );
 function lct_admin_bar_on_bottom_back_css() {
-	global $lct_G;
+	global $lct_g;
 	$user = wp_get_current_user();
 
-	if( get_the_author_meta( $lct_G['lct_us'] . '_back', $user->ID ) )
+	if( get_the_author_meta( $lct_g->lct_us . '_back', $user->ID ) )
 		wp_enqueue_style( 'bottom-back', plugins_url( 'css/back.css', __FILE__ ) );
 
 	wp_enqueue_style( 'lct-admin-bar-on-bottom-profile', plugins_url( 'css/profile.css', __FILE__ ) );
 }
 
 
-add_action( 'wp_enqueue_scripts', $lct_G['lct_us'] . '_front_css' );
+add_action( 'wp_enqueue_scripts', $lct_g->lct_us . '_front_css' );
 function lct_admin_bar_on_bottom_front_css() {
-	global $lct_G;
+	global $lct_g;
 	$user = wp_get_current_user();
 
-	if( get_the_author_meta( $lct_G['lct_us'] . '_front', $user->ID ) )
-		wp_enqueue_style( $lct_G['lct_us'] . '_front', plugins_url( 'css/front.css', __FILE__ ) );
+	if( get_the_author_meta( $lct_g->lct_us . '_front', $user->ID ) )
+		wp_enqueue_style( $lct_g->lct_us . '_front', plugins_url( 'css/front.css', __FILE__ ) );
 }
 
 
-add_action( 'show_user_profile', $lct_G['lct_us'] . '_extra_profile_fields' );
-add_action( 'edit_user_profile', $lct_G['lct_us'] . '_extra_profile_fields' );
+add_action( 'show_user_profile', $lct_g->lct_us . '_extra_profile_fields' );
+add_action( 'edit_user_profile', $lct_g->lct_us . '_extra_profile_fields' );
 function lct_admin_bar_on_bottom_extra_profile_fields( $user ) {
-	global $lct_G; ?>
+	global $lct_g; ?>
 
 	<div id="lct-admin-bar-on-bottom">
 		<h3>Admin Bar Settings (wpadminbar)</h3>
@@ -65,17 +69,17 @@ function lct_admin_bar_on_bottom_extra_profile_fields( $user ) {
 
 			<table class="form-table">
 				<tr>
-					<th><label for="<?php echo $lct_G['lct_us']; ?>_front">Front-end</label></th>
+					<th><label for="<?php echo $lct_g->lct_us; ?>_front">Front-end</label></th>
 					<td>
-						<?php get_the_author_meta( $lct_G['lct_us'] . '_front', $user->ID ) ? $checked = 'checked="checked"' : $checked = ''; ?>
-						<input type="checkbox" name="<?php echo $lct_G['lct_us']; ?>_front" value="1" <?php echo $checked; ?> />
+						<?php get_the_author_meta( $lct_g->lct_us . '_front', $user->ID ) ? $checked = 'checked="checked"' : $checked = ''; ?>
+						<input type="checkbox" name="<?php echo $lct_g->lct_us; ?>_front" value="1" <?php echo $checked; ?> />
 					</td>
 				</tr>
 				<tr>
-					<th><label for="<?php echo $lct_G['lct_us']; ?>_back">Back-end</label></th>
+					<th><label for="<?php echo $lct_g->lct_us; ?>_back">Back-end</label></th>
 					<td>
-						<?php get_the_author_meta( $lct_G['lct_us'] . '_back', $user->ID ) ? $checked = 'checked="checked"' : $checked = ''; ?>
-						<input type="checkbox" name="<?php echo $lct_G['lct_us']; ?>_back" value="1" <?php echo $checked; ?> />
+						<?php get_the_author_meta( $lct_g->lct_us . '_back', $user->ID ) ? $checked = 'checked="checked"' : $checked = ''; ?>
+						<input type="checkbox" name="<?php echo $lct_g->lct_us; ?>_back" value="1" <?php echo $checked; ?> />
 					</td>
 				</tr>
 			</table>
@@ -84,12 +88,12 @@ function lct_admin_bar_on_bottom_extra_profile_fields( $user ) {
 <?php }
 
 
-add_action( 'personal_options_update', 'save_' . $lct_G['lct_us'] . '_extra_profile_fields' );
-add_action( 'edit_user_profile_update', 'save_' . $lct_G['lct_us'] . '_extra_profile_fields' );
+add_action( 'personal_options_update', 'save_' . $lct_g->lct_us . '_extra_profile_fields' );
+add_action( 'edit_user_profile_update', 'save_' . $lct_g->lct_us . '_extra_profile_fields' );
 function save_lct_admin_bar_on_bottom_extra_profile_fields( $user_id ) {
-	global $lct_G;
+	global $lct_g;
 	if( ! current_user_can( 'edit_user', $user_id ) ) return false;
 
-	update_usermeta( $user_id, $lct_G['lct_us'] . '_front', $_POST[$lct_G['lct_us'] . '_front'] );
-	update_usermeta( $user_id, $lct_G['lct_us'] . '_back', $_POST[$lct_G['lct_us'] . '_back'] );
+	update_usermeta( $user_id, $lct_g->lct_us . '_front', $_POST[$lct_g->lct_us . '_front'] );
+	update_usermeta( $user_id, $lct_g->lct_us . '_back', $_POST[$lct_g->lct_us . '_back'] );
 } ?>
